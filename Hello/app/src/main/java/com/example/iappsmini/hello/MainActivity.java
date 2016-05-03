2a232e840d0a4650f87b266dd39fe52fb6e04e3e
iAPPS Mini <iappsmini@iAPPSs-Mac-mini.local>
2016-05-03 19:04:54 +0800

    Hello as Login Screen
    Hello as Login Screen

--- Hello/app/src/main/java/com/example/iappsmini/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/iappsmini/hello/MainActivity.java
@@ -9,24 +9,17 @@ import android.view.View;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    Button btn_Hello;
-    TextView txt_Hello;
-    String hello;
+    Button btn_Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_Hello = (Button) findViewById(R.id.btn_Hello);
-        txt_Hello = (TextView) findViewById(R.id.txt_Hello);
-
-        hello="Hello";
-        btn_Hello.setOnClickListener(this);
-
+        btn_Login = (Button) findViewById(R.id.btn_Login);
     }
 
     public void onClick(View view){
-        txt_Hello.setText(hello);
+
     }
 }
